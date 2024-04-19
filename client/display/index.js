d59c493617783ac90da5f1fa75d1682557bd5c47
Trey Tomes <george.tomes@cityelectricsupply.com>
2024-04-19 07:40:56 -0500

    Circle drawing and bouncing ball demo.

--- client/display/index.js
+++ client/display/index.js
@@ -29,6 +29,119 @@ export class Color {
 };
 
 /**
+ * Clear the screen to a color.
+ * 
+ * @param {Color}} color The color to clear to.
+ */
+export const clear = (color) => {
+  for (let y = 0; y < getHeight(); y++) {
+    for (let x = 0; x < getWidth(); x++) {
+      setPixel(x, y, color);
+    }
+  }
+}
+
+/**
+ * Draw an arbitrary line across the screen.
+ * 
+ * @param {number} x1 The starting x-value.
+ * @param {number} y1 The starting y-value.
+ * @param {number} x2 The ending x-value.
+ * @param {number} y2 The ending y-value.
+ * @param {{r: number, g: number, b: number}} color The line color.
+ */
+export const drawLine = (x1, y1, x2, y2, color) => {
+  x1 = Math.floor(x1);
+  x2 = Math.floor(x2);
+  y1 = Math.floor(y1);
+  y2 = Math.floor(y2);
+
+  if (x1 === x2 && y1 === y2) {
+    setPixel(x1, y1, color);
+    return;
+  }
+  
+  const xMin = Math.min(x1, x2);
+  const xMax = Math.max(x1, x2);
+  const yMin = Math.min(y1, y2);
+  const yMax = Math.max(y1, y2);
+  if (xMin === xMax && yMin !== yMax) {
+    for (let y = yMin; y <= yMax; y++) {
+      setPixel(xMin, y, color);
+    }
+  } else if (xMin !== xMax && yMin === yMax) {
+    for (let x = xMin; x <= xMax; x++) {
+      setPixel(x, yMin, color);
+    }
+  } else {
+    let dx = Math.abs(x2 - x1);
+    let sx = (x1 < x2) ? 1 : -1;
+    let dy = Math.abs(y2 - y1);
+    dy = -dy;
+    let sy = (y1 < y2) ? 1 : -1;
+    let err = dx + dy;
+  
+    while (true) {
+      setPixel(x1, y1, color);
+      if ((x1 == x2) && (y1 == y2)) break;
+      let e2 = err << 1;
+      if (e2 >= dy) {
+        if (x1 === x2) break
+        err += dy;
+        x1 += sx;
+      }
+      if (e2 <= dx) {
+        if (y1 === y2) break;
+        err += dx;
+        y1 += sy;
+      }
+    }
+  }
+}
+
+/**
+ * Function for circle-generation using Bresenham's algorithm.
+ * 
+ * @param {number} xc The center x-value of the circle.
+ * @param {number} yc The center y-value of the circle.
+ * @param {number} r The radius of the circle.
+ * @param {Color} c The color of the circle.
+ */
+export const drawCircle = (xc, yc, r, c) => {
+  xc = Math.floor(xc);
+  yc = Math.floor(yc);
+  r = Math.floor(r);
+  
+  const drawOctants = (x, y) => {
+    setPixel(xc + x, yc + y, c);
+    setPixel(xc - x, yc + y, c);
+    setPixel(xc + x, yc - y, c);
+    setPixel(xc - x, yc - y, c);
+    setPixel(xc + y, yc + x, c);
+    setPixel(xc - y, yc + x, c);
+    setPixel(xc + y, yc - x, c);
+    setPixel(xc - y, yc - x, c);
+  }
+
+  let x = 0, y = r;
+  let d = 3 - 2 * r;
+  drawOctants(x, y);
+  while (y >= x) {
+    // For each pixel we will draw all eight pixels.
+    x++;
+
+    // Check for decision parameter and correspondingly update d, x, y.
+    if (d > 0) {
+      y--;
+      d = d + 4 * (x - y) + 10;
+    } else {
+      d = d + 4 * x + 6;
+    }
+    drawOctants(x, y);
+  }
+}
+
+/**
  * Write a single pixel to the pixel buffer.
  * This change will not be visible until the next `postRender` call.
  * 
--- client/index.js
+++ client/index.js
@@ -1,4 +1,4 @@
-import { getWidth, getHeight, createContext, postRender, setPixel, Color } from './display/index.js'
+import { getWidth, getHeight, createContext, postRender, setPixel, Color, drawCircle, clear } from './display/index.js'
 import { Image } from './image.js'
 
 const image = new Image(await window.api.gfx.getTiles())
@@ -29,122 +29,54 @@ function loadTiles() {
   // }
 }
 
-/**
- * Clear the screen to a color.
- * 
- * @param {Color}} color The color to clear to.
- */
-const clear = (color) => {
-  for (let y = 0; y < getHeight(); y++) {
-    for (let x = 0; x < getWidth(); x++) {
-      setPixel(x, y, color);
+class Ball {
+  constructor(x = undefined, y = undefined, r = undefined, color = undefined) {
+    this.x = x ?? Math.random() * getWidth();
+    this.y = y ?? Math.random() * getHeight();
+    this.r = r ?? Math.random() * 20 + 10;
+    this.color = color ?? new Color(Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255));
+    
+    this.dx = Math.random() * 2 - 1;
+    this.dy = Math.random() * 2 - 1;
+  }
+
+  draw() {
+    drawCircle(this.x, this.y, this.r, this.color);
+  }
+
+  update() {
+    this.x += this.dx;
+    this.y += this.dy;
+
+    if (this.x - this.r <= 0 || this.x + this.r > getWidth()) {
+      this.dx = -this.dx;
+    }
+    if (this.y - this.r <= 0 || this.y + this.r > getHeight()) {
+      this.dy = -this.dy;
     }
   }
 }
 
-/**
- * Draw an arbitrary line across the screen.
- * 
- * @param {number} x1 The starting x-value.
- * @param {number} y1 The starting y-value.
- * @param {number} x2 The ending x-value.
- * @param {number} y2 The ending y-value.
- * @param {{r: number, g: number, b: number}} color The line color.
- */
-const drawLine = (x1, y1, x2, y2, color) => {
-  x1 = Math.floor(x1);
-  x2 = Math.floor(x2);
-  y1 = Math.floor(y1);
-  y2 = Math.floor(y2);
-
-  if (x1 === x2 && y1 === y2) {
-    setPixel(x1, y1, color);
-    return;
-  }
-  
-  const xMin = Math.min(x1, x2);
-  const xMax = Math.max(x1, x2);
-  const yMin = Math.min(y1, y2);
-  const yMax = Math.max(y1, y2);
-  if (xMin === xMax && yMin !== yMax) {
-    for (let y = yMin; y <= yMax; y++) {
-      setPixel(xMin, y, color);
-    }
-  } else if (xMin !== xMax && yMin === yMax) {
-    for (let x = xMin; x <= xMax; x++) {
-      setPixel(x, yMin, color);
-    }
-  } else {
-    let dx = Math.abs(x2 - x1);
-    let sx = (x1 < x2) ? 1 : -1;
-    let dy = Math.abs(y2 - y1);
-    dy = -dy;
-    let sy = (y1 < y2) ? 1 : -1;
-    let err = dx + dy;
-  
-    while (true) {
-      setPixel(x1, y1, color);
-      if ((x1 == x2) && (y1 == y2)) break;
-      let e2 = err << 1;
-      if (e2 >= dy) {
-        if (x1 === x2) break
-        err += dy;
-        x1 += sx;
-      }
-      if (e2 <= dx) {
-        if (y1 === y2) break;
-        err += dx;
-        y1 += sy;
-      }
-    }
-  }
-}
+const sprites = []
 
 const render = (totalTime) => {
-  // for (let y = 0; y < getWidth(); y++) {
-  //   for (let x = 0; x < getHeight(); x++) {
-  //     setPixel(x, y, { r: x ^ y, g: x & y, b: x | y })
-  //   }
-  // }
+  clear(new Color(16, 16, 64));
 
-  // setPixel(100, 100, { r: 255, g: 255, b: 0 })
-
-  // console.log(image.width, image.height)
-  // for (let y = 0; y < image.height; y++) {
-  //   for (let x = 0; x < image.width; x++) {
-  //     setPixel(x, y, image.getPixel(x, y))
-  //   }
-  // }
-  
-  clear({ r: 0, g: 0, b: 64, a: 255 });
-
-  // A single pixel.
-  drawLine(100, 100, 100, 100, new Color(255, 0, 0));
-  
-  // A vertical line.
-  drawLine(120, 110, 120, 150, new Color(255, 0, 255));
-  
-  // A horizontal line.
-  drawLine(120, 110, 150, 110, new Color(0, 255, 255));
-
-  const centerX = getWidth() / 2;
-  const centerY = getHeight() / 2;
-  const radius = Math.min(getWidth(), getHeight()) / 2;
-
-  // radians = degrees * PI / 180
-
-  const radians = (totalTime / 16) * Math.PI / 180;
-  const x = centerX + Math.cos(radians) * radius;
-  const y = centerY + Math.sin(radians) * radius;
-  drawLine(centerX, centerY, x, y, new Color(255, 255, 0));
+  for (let n = 0; n < sprites.length; n++) {
+    sprites[n].draw();
+  }
 }
 
 let lastUpdateTime = 0
-const UPDATE_INTERVAL = 1000
+const UPDATE_INTERVAL = 1000 / 60;
 const onRenderFrame = (totalTime) => {
   if (totalTime - lastUpdateTime >= UPDATE_INTERVAL) {
-    console.log('Update!')
-    lastUpdateTime = totalTime
+    // console.log('Update!')
+    lastUpdateTime = totalTime;
+
+    for (let n = 0; n < sprites.length; n++) {
+      sprites[n].update();
+    }
   }
 
   render(totalTime)
@@ -158,6 +90,11 @@ console.log('Here we go.')
 console.log(await window.api.sample.ping())
 
 await createContext(256, 256);
+
+const NUM_BALLS = 10000;
+for (let n = 0; n < NUM_BALLS; n++) {
+  sprites.push(new Ball());
+}
 
 // Begin the render loop.
 requestAnimationFrame(onRenderFrame)
