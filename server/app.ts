78afcc96361e1074f08aeca12c0a2cc11ef3c62a
Trey Tomes <george.tomes@cityelectricsupply.com>
2024-05-13 14:29:09 -0500

    fix: remove defunct reloader.

--- server/app.ts
+++ server/app.ts
@@ -3,18 +3,8 @@ import { config } from './config';
 
 import Main from './main';
 import { app, BrowserWindow } from 'electron';
-import { Environment } from './models';
 
 console.log('Environment:', config.get('environment'));
 console.log('Debug:', config.get('debug'));
-
-if (config.get('environment') == Environment.dev) {
-  try {
-    require('electron-reloader')(module, {
-      debug: true,
-      watchRenderer: true
-    });
-  } catch (_) { console.log('Error'); }
-}
 
 Main.main(app, BrowserWindow);
