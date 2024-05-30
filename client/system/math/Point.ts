768ac357a543097aae5d8dab3c27d17c61ef3465
Trey Tomes <george.tomes@cityelectricsupply.com>
2024-05-30 08:03:02 -0500

    fix: clean up commentary

--- client/system/math/Point.ts
+++ client/system/math/Point.ts
@@ -1,42 +1,32 @@
+import Rectangle from './Rectangle';
+
 export default class Point {
   readonly x: number;
   readonly y: number;
 
-  /**
-   * @param {number} x The x-position.
-   * @param {number} y The y-position.
-   */
   constructor(x: number, y: number) {
     this.x = x;
     this.y = y;
   }
 
-  /**
-   * @returns {Point}
-   */
   static get zero(): Point {
     return new Point(0, 0);
   }
 
-  /**
-   * @returns {Point}
-   */
   static get unitX(): Point {
     return new Point(1, 0);
   }
 
-  /**
-   * @returns {Point}
-   */
   static get unitY(): Point {
     return new Point(0, 1);
   }
 
-  /**
-   * @returns {Point}
-   */
   get negate(): Point {
     return new Point(-this.x, -this.y);
+  }
+
+  get floor(): Point {
+    return new Point(Math.floor(this.x), Math.floor(this.y));
   }
 
   /**
@@ -49,11 +39,22 @@ export default class Point {
     return new Point(this.x + p.x, this.y + p.y);
   }
 
+  subtract(p: Point): Point {
+    return new Point(this.x - p.x, this.y - p.y);
+  }
+
   /**
    * @param {number} value Scalar value to multiply by.
    * @returns {Point} A new point.
    */
   multiply(value: number): Point {
     return new Point(this.x * value, this.y * value);
+  }
+
+  clamp(bounds: Rectangle): Point {
+    return new Point(
+      Math.max(Math.min(this.x, bounds.right), bounds.left),
+      Math.max(Math.min(this.y, bounds.bottom), bounds.top)
+    );
   }
 }
